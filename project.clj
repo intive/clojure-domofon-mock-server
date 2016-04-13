d509cd9c79b2dc3cc77aad0869a19f0668bc3f95
Łukasz Indykiewicz <lukasz.indykiewicz@blstream.com>
2016-04-13 10:22:07 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject domofon-mock-server "0.1.0-SNAPSHOT"
+  :description "Mock server that supports domofon-tck"
+  :url "https://github.com/blstream/domofon-mock-server"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.4.0"]
+                 [ring/ring-json "0.4.0"]
+                 [ring/ring-defaults "0.1.5"]]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler domofon-mock-server.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}})
